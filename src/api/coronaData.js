41a29c01c17703255f19401a7831fc7dbd6846ce
Inzamul Haque <inzamul119@gmail.com>
2022-02-04 00:54:36 +0600

    implement modular client for api call

--- src/api/coronaData.js
+++ src/api/coronaData.js
@@ -1,6 +1,8 @@
-import axios from 'axios';
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-alert */
 import dayjs from 'dayjs';
 import { calculateToChart } from '../utils/calculate';
+import client from './client';
 
 const baseUrl = `https://${import.meta.env.VITE_API_HOST}/`;
 
@@ -13,64 +15,55 @@ const defaultConfig = {
 
 //
 export const getCountries = async () => {
-  const url = `${baseUrl}countries`;
-  let result = '';
+  let result;
   try {
-    const response = await axios.get(url, defaultConfig);
-    result = response.data.response;
+    result = await client.get('/countries');
   } catch (e) {
-    // eslint-disable-next-line no-alert
+    //
     alert(e.message);
   }
   return result;
 };
 //
 export const getCovidData = async (country) => {
-  const url = `${baseUrl}statistics`;
-  const parameters = {
+  let result;
+  // query parameters
+  const query = {
     params: {
       country: country === 'global' ? null : country,
     },
   };
-  let result = '';
   try {
-    const response = await axios.get(url, { ...defaultConfig, ...parameters });
-    result = response.data.response;
+    result = await client.get('/statistics', query);
   } catch (e) {
     //
+    alert(e.message);
   }
   return result;
 };
 //
 export const getPerDaysData = async (days, country) => {
-  const targetCountry = country === 'global' ? 'all' : country;
   const chartData = [];
+  //
 
   const now = dayjs();
   for (let i = 0; i < days; i += 1) {
     const date = now.subtract(i, 'd').format('YYYY-MM-DD');
     //
-    // api url
-    const url = `${baseUrl}history`;
     // api parameters
-    const parameters = {
+    const query = {
       params: {
-        country: targetCountry,
+        country: country === 'global' ? 'all' : country,
         day: date,
       },
     };
     // calling api per days
     try {
-      // eslint-disable-next-line no-await-in-loop
-      const response = await axios.get(url, {
-        ...defaultConfig,
-        ...parameters,
-      });
-      const target = response.data.response[0];
-      chartData.push(calculateToChart(target));
+      const response = await client.get('/history', query);
+      chartData.push(calculateToChart(response[0]));
     } catch (e) {
       //
-      return null;
+      alert(e);
     }
   }
 
