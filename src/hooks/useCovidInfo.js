44179ef2558a1e50c11b8f45062e4c49a7a95fe2
Inzamul Haque <inzamul119@gmail.com>
2022-02-04 12:36:51 +0600

    create useCovidInfo to pull api data

---
+++ src/hooks/useCovidInfo.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import client from '../api/client';
+import calculateStats from '../utils/calculate';
+
+//
+const END_URL = '/statistics';
+//
+
+const useCovidInfo = (country) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  const request = async () => {
+    const query = {
+      params: {
+        country: country === 'global' ? null : country,
+      },
+    };
+    //
+    setLoading(true);
+    try {
+      const response = await client.get(END_URL, query);
+      const info = calculateStats(response);
+      setData(info);
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    data,
+    error,
+    loading,
+    request,
+  };
+};
+
+export default useCovidInfo;
